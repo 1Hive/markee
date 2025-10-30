57f09b3eb3d152a35f4c9a95c62e31bad4de5452
Paul2 <paulglavin2@gmail.com>
2025-10-29 20:05:09 -0700

    Update index.ts

--- frontend/types/index.ts
+++ frontend/types/index.ts
@@ -1,1 +1,13 @@
+export interface Markee {
+  address: string
+  owner: string
+  message: string
+  totalFundsAdded: bigint
+  chainId: number
+  pricingStrategy: string
+}
 
+export interface MarkeeWithChain extends Markee {
+  chainName: string
+  chainColor: string
+}
