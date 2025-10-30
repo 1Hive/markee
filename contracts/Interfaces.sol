a974da37566fed5a837373956845d7e7384650c8
Paul2 <paulglavin2@gmail.com>
2025-10-29 19:39:27 -0700

    Update Interfaces.sol

--- contracts/Interfaces.sol
+++ contracts/Interfaces.sol
@@ -1,1 +1,15 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.23;
 
+/// @notice Interface for Juicebox Multi Terminal V5
+interface IJBMultiTerminal {
+    function pay(
+        uint256 projectId,
+        address token,
+        uint256 amount,
+        address beneficiary,
+        uint256 minReturnedTokens,
+        string calldata memo,
+        bytes calldata metadata
+    ) external payable returns (uint256 beneficiaryTokenCount);
+}
