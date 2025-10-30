8b205b4d235a53073e3acbf5125eec59b147fb2b
Paul2 <paulglavin2@gmail.com>
2025-10-29 19:33:42 -0700

    Delete contracts directory

--- contracts/src/IPricingModule.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: AGPL-3.0-or-later
-pragma solidity ^0.8.19;
-
-interface IPricingModule {
-    function getCurrentPrice(address marquee) external view returns (uint256);
-    function updatePrice(address marquee, uint256 newAmount) external;
-    function initializePrice(address marquee, uint256 initialPrice) external;
-}
