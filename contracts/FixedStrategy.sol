083faf30b4cf0869dae0a78569695dead485a670
Paul2 <paulglavin2@gmail.com>
2025-11-07 16:10:21 -0800

    Update FixedStrategy.sol

--- contracts/FixedStrategy.sol
+++ contracts/FixedStrategy.sol
@@ -18,15 +18,18 @@ contract FixedStrategy {
     address public owner;
     uint256 public price;
     uint256 public maxMessageLength;
+    uint256 public maxNameLength;
     
     // Events
     event MessageChanged(
         address indexed changedBy,
         string newMessage,
+        string name,
         uint256 pricePaid
     );
     event PriceUpdated(uint256 oldPrice, uint256 newPrice);
     event MaxMessageLengthUpdated(uint256 oldLength, uint256 newLength);
+    event MaxNameLengthUpdated(uint256 oldLength, uint256 newLength);
     event PricingStrategyChanged(address indexed newStrategy);
     event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
     
@@ -34,31 +37,39 @@ contract FixedStrategy {
     /// @param _revNetTerminal Address of the Juicebox terminal for RevNet payments
     /// @param _revNetProjectId The RevNet project ID for Markee
     /// @param _initialMessage The initial message for the Markee
+    /// @param _initialName The initial name for the Markee creator
     /// @param _price The fixed price (in wei) anyone must pay to change the message
     /// @param _maxMessageLength The maximum message length
+    /// @param _maxNameLength The maximum name length
     constructor(
         address _revNetTerminal,
         uint256 _revNetProjectId,
         string memory _initialMessage,
+        string memory _initialName,
         uint256 _price,
-        uint256 _maxMessageLength
+        uint256 _maxMessageLength,
+        uint256 _maxNameLength
     ) {
         require(_revNetTerminal != address(0), "RevNet terminal cannot be zero address");
         require(_price > 0, "Price must be greater than zero");
         require(_maxMessageLength > 0, "Maximum message length must be greater than zero");
+        require(_maxNameLength > 0, "Maximum name length must be greater than zero");
         require(bytes(_initialMessage).length <= _maxMessageLength, "Message exceeds maximum length");
+        require(bytes(_initialName).length <= _maxNameLength, "Name exceeds maximum length");
         
         revNetTerminal = _revNetTerminal;
         revNetProjectId = _revNetProjectId;
         owner = msg.sender;
         price = _price;
         maxMessageLength = _maxMessageLength;
+        maxNameLength = _maxNameLength;
         
         // Deploy the Markee contract
         Markee markee = new Markee(
             msg.sender,           // owner = the deployer
             address(this),        // pricingStrategy = this contract
             _initialMessage,      // initial message
+            _initialName,         // initial name
             0                     // no initial funds
         );
         
@@ -67,7 +78,8 @@ contract FixedStrategy {
     
     /// @notice Allows anyone to pay the fixed price to change the message
     /// @param _newMessage The new message to set
-    function changeMessage(string calldata _newMessage) 
+    /// @param _name The name of the person changing the message
+    function changeMessage(string calldata _newMessage, string calldata _name) 
         external 
         payable 
     {
@@ -77,11 +89,17 @@ contract FixedStrategy {
         // Check message length
         require(bytes(_newMessage).length <= maxMessageLength, "Message exceeds maximum length");
         
+        // Check name length
+        require(bytes(_name).length <= maxNameLength, "Name exceeds maximum length");
+        
         // Get reference to the Markee
         Markee markee = Markee(markeeAddress);
         
         // Update the message
         markee.setMessage(_newMessage);
+        
+        // Update the name
+        markee.setName(_name);
         
         // Add funds to the Markee's total
         markee.addFunds(msg.value);
@@ -97,7 +115,7 @@ contract FixedStrategy {
             ""                    // metadata
         );
         
-        emit MessageChanged(msg.sender, _newMessage, msg.value);
+        emit MessageChanged(msg.sender, _newMessage, _name, msg.value);
     }
     
     /// @notice Allows the owner to update the fixed price
@@ -122,6 +140,18 @@ contract FixedStrategy {
         maxMessageLength = _newMaxLength;
         
         emit MaxMessageLengthUpdated(oldLength, _newMaxLength);
+    }
+    
+    /// @notice Allows the owner to update the maximum name length
+    /// @param _newMaxLength The new maximum length in characters
+    function setMaxNameLength(uint256 _newMaxLength) external {
+        require(msg.sender == owner, "Only owner can set maximum name length");
+        require(_newMaxLength > 0, "Maximum name length must be greater than zero");
+        
+        uint256 oldLength = maxNameLength;
+        maxNameLength = _newMaxLength;
+        
+        emit MaxNameLengthUpdated(oldLength, _newMaxLength);
     }
     
     /// @notice Allows the owner to change the pricing strategy
