3db000f41dbabea28060705e50eec6ebee5f0df7
Paul2 <paulglavin2@gmail.com>
2026-01-09 13:01:37 -0800

    Update FixedPriceModal.tsx

--- frontend/components/modals/FixedPriceModal.tsx
+++ frontend/components/modals/FixedPriceModal.tsx
@@ -66,7 +66,7 @@ export function FixedPriceModal({
 
   // Close modal & refresh after success
   useEffect(() => {
-    if (isSuccess) {
+    if (isSuccess && isOpen) {
       setTimeout(() => {
         if (onSuccess) onSuccess()
         onClose()
