87b04c8e537d4acf77e10edc18cd20874d589b93
Paul2 <paulglavin2@gmail.com>
2026-02-11 09:56:32 -0800

    Add moderation module index file
    Create moderation module barrel file for exports.

---
+++ frontend/components/moderation/index.ts
@@ -0,0 +1,10 @@
+/**
+ * Moderation Module
+ * 
+ * Import everything from this barrel:
+ *   import { ModerationProvider, ModeratedContent, FlagButton, useModeration } from '@/components/moderation'
+ */
+
+export { ModerationProvider, useModeration } from './ModerationProvider'
+export { ModeratedContent } from './ModeratedContent'
+export { FlagButton } from './FlagButton'
